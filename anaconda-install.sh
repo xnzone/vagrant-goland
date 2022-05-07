9bb6498376aea5673c50f42d0ee093ad7313cff7
xnzone <xnzone@qq.com>
2022-05-07 18:26:37 +0800

    update goland install

--- anaconda-install.sh
+++ anaconda-install.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 set -e
-wget https://mirrors.tuna.tsinghua.edu.cn/anaconda/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
+wget http://mirrors.tuna.tsinghua.edu.cn/anaconda/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
 
 bash ~/miniconda.sh -b -p $HOME/.conda
 ~/.conda/bin/conda init $(echo $SHELL | awk -F '/' '{print $NF}') 
 echo 'Successfully installed miniconda...'
 echo -n 'conda version:'
 ~/.conda/bin/conda --version
+rm -rf ~/miniconda.sh
 echo -e '\n'
---
+++ goland-install.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+echo "Using aliyun apt mirrors"
+sudo sed -i "s/archive.ubuntu.com/mirrors.aliyun.com/g" /etc/apt/sources.list
+sudo sed -i "s/security.ubuntu.com/mirrors.aliyun.com/g" /etc/apt/sources.list
+
+echo "Installing dependencies ..."
+sudo apt update
+sudo apt install -y curl ca-certificates build-essential git  less libxext6 libxrender1 libxtst6 libfreetype6 libxi6
+sudo apt install -y python3 python3-pip
+
+echo "Installing go1.16.2"
+wget https://golang.google.cn/dl/go1.16.2.linux-amd64.tar.gz
+sudo tar -zxvf go1.16.2.linux-amd64.tar.gz -C /usr/local/
+sudo tee -a /etc/profile > /dev/null << EOT
+export GOROOT=/usr/local/go
+export GOPATH=/home/vagrant/go
+export PATH=\$PATH:\$GOROOT/bin:\$GOPATH/bin
+EOT
+
+# If someone use zsh, set PATH to ~/.zprofile
+tee -a ~/.zprofile > /dev/null << EOT
+export GOROOT=/usr/local/go
+export GOPATH=/home/vagrant/go
+export PATH=\$PATH:\$GOROOT/bin:\$GOPATH/bin
+EOT
+
+echo "Using tsinghua pip mirros"
+mkdir .pip && touch .pip/pip.conf
+tee -a .pip/pip.conf > /dev/null << EOT
+[global]
+index-url = https://pypi.tuna.tsinghua.edu.cn/simple
+[install]
+trusted-host = pypi.tuna.tsinghua.edu.cn
+EOT
+
+echo "Installing JetBrains Goland ..."
+# pip3 install -U pip setuptools
+pip3 install projector-installer==1.5.0
+projector --accept-license autoinstall --ide-name "GoLand 2020.3.2" --config-name goland
+
+echo "Cleaning up ..."
+sudo apt-get autoclean -y
+sudo apt-get clean -y
+sudo rm -rf /var/lib/apt/lists/* \
+    /var/cache/apt/pkgcache.bin \
+    /var/cache/apt/srcpkgcache.bin \
+    /home/vagrant/go1.16.2*
