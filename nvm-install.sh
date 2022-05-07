9ea83ba2a207d88cc9cccf50942a8d9a95f14282
xnzone <xnzone@qq.com>
2022-05-07 13:54:39 +0800

    update nvm install

--- nvm-install.sh
+++ nvm-install.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 
+sh -c "$(curl -fsSL https://gitee.com/mirrors/nvm/raw/master/install.sh \
+    | sed 's|^REPO=.*|REPO=${REPO:-mirrors/nvm}|g' \
+    | sed 's|^REMOTE=.*|REMOTE=${REMOTE:-https://gitee.com/${REPO}.git}|g')"
